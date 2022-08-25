d73d4b86e5a200da9c2887d824cadf1a5ac05a19
Ian Greenough <ian.greenough.developer@gmail.com>
2022-08-25 19:23:43 +0200

    Adds comment

--- pages/user/[id].tsx
+++ pages/user/[id].tsx
@@ -1,30 +1,24 @@
-import {
-  GetStaticPaths,
-  GetStaticProps,
-  GetStaticPropsContext,
-  GetStaticPropsResult,
-} from "next";
 import Head from "next/head";
 import { useRouter } from "next/router";
-import { ParsedUrlQuery } from "querystring";
 import { useEffect } from "react";
 import { useSelector } from "react-redux";
 
-import { getItemData, getAllItemIds } from "../../src/service";
 import { selectUser } from "../../src/state";
 import { Item } from "../../src/type";
 import { LayoutAuth } from "../../src/view/component";
-import { ItemDetailPage } from "../../src/view/page";
 
 type ItemRouteType = {
   item: Item;
 };
 
 export default function ItemDetailRoute({ item }: ItemRouteType) {
-  const router = useRouter();
-  const user = useSelector(selectUser);
 
   const { title } = item;
+
+  // Creat Auth redirect component
+
+  const router = useRouter();
+  const user = useSelector(selectUser);
 
   useEffect(() => {
     if (!user) {
@@ -37,31 +31,7 @@ export default function ItemDetailRoute({ item }: ItemRouteType) {
       <Head>
         <title>{title}</title>
       </Head>
-      <ItemDetailPage title={title} />
+      User Page
     </LayoutAuth>
   );
 }
-
-// SSR
-
-export const getStaticPaths: GetStaticPaths = async () => {
-  const paths = getAllItemIds();
-  return {
-    paths,
-    fallback: false,
-  };
-};
-
-export const getStaticProps: GetStaticProps = async ({
-  params,
-}: GetStaticPropsContext<ParsedUrlQuery>): Promise<
-  GetStaticPropsResult<ItemRouteType>
-> => {
-  const id = params?.id || ""; // There will always be an id due to routing
-  const item: Item = await getItemData(id);
-  return {
-    props: {
-      item,
-    },
-  };
-};
